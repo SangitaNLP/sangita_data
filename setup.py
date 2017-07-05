afc3f29e31ecd921bf9a33e155a8dac78d359c6e
djokester <samriddhidjokestersinha@gmail.com>
2017-07-06 04:50:39 +0530

    Final Changes Hopefully

--- setup.py
+++ setup.py
@@ -17,17 +17,17 @@ if sys.version_info[:2] < (3, 5):
     print(('lingatagger requires Python version 3.5 or later (%d.%d detected).' %sys.version_info[:2]))
     sys.exit(-1)
 
-sys.path.insert(0, 'sangita-data')
+sys.path.insert(0, 'sangita_data')
 
 
 if __name__ == "__main__":
     setup(
-        name = "sangita-data",
+        name = "sangita_data",
         version = "1.0",
         author = "Samriddhi Sinha",
         author_email = "samridhhisinha.iitkgp@gmail.com",
         description = "Data and Corpus pertaining to Sangita",
-        url='https://github.com/djokester/sangita-data',
+        url='https://github.com/djokester/sangita_data',
         keywords= ['nlp', 'hindi', 'linguistics'],
         packages = ['sangita_data','sangita_data.hindi', 'sangita_data.hindi.sentences'],
         license = 'Apache License'
