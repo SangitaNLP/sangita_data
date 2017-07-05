1019c5bf123d1207f27b7d1abb7c375c908996fd
djokester <samriddhidjokestersinha@gmail.com>
2017-07-05 23:35:41 +0530

    Added An Aggregator

---
+++ sangita-data/hindi/sentences/loadsent.py
@@ -0,0 +1,97 @@
+import sent0
+import sent1
+import sent2
+import sent3
+import sent4
+import sent5
+import sent6
+import sent7
+import sent8
+import sent9
+import sent10
+import sent11
+import sent12
+import sent13
+import sent14
+import sent15
+import sent16
+import sent17
+import sent18
+import sent19
+import sent20
+import sent21
+import sent22
+import sent23
+import sent24
+import sent25
+import sent26
+import sent27
+import sent28
+import sent29
+import sent30
+import sent31
+import sent32
+import sent33
+import sent34
+import sent35
+import sent36
+import sent37
+import sent38
+import sent39
+import sent40
+import sent41
+import sent42
+import sent43
+import sent44
+import sent45
+import sent46
+
+def drawlist():
+    sent = sent0.drawlist()
+    sent = sent + sent1.drawlist()
+    sent = sent + sent2.drawlist()
+    sent = sent + sent3.drawlist()
+    sent = sent + sent4.drawlist()
+    sent = sent + sent5.drawlist()
+    sent = sent + sent6.drawlist()
+    sent = sent + sent7.drawlist()
+    sent = sent + sent8.drawlist()
+    sent = sent + sent9.drawlist()
+    sent = sent + sent10.drawlist()
+    sent = sent + sent11.drawlist()
+    sent = sent + sent12.drawlist()
+    sent = sent + sent13.drawlist()
+    sent = sent + sent14.drawlist()
+    sent = sent + sent15.drawlist()
+    sent = sent + sent16.drawlist()
+    sent = sent + sent17.drawlist()
+    sent = sent + sent18.drawlist()
+    sent = sent + sent19.drawlist()
+    sent = sent + sent20.drawlist()
+    sent = sent + sent21.drawlist()
+    sent = sent + sent22.drawlist()
+    sent = sent + sent23.drawlist()
+    sent = sent + sent24.drawlist()
+    sent = sent + sent25.drawlist()
+    sent = sent + sent26.drawlist()
+    sent = sent + sent27.drawlist()
+    sent = sent + sent28.drawlist()
+    sent = sent + sent29.drawlist()
+    sent = sent + sent30.drawlist()
+    sent = sent + sent31.drawlist()
+    sent = sent + sent32.drawlist()
+    sent = sent + sent33.drawlist()
+    sent = sent + sent34.drawlist()
+    sent = sent + sent35.drawlist()
+    sent = sent + sent36.drawlist()
+    sent = sent + sent37.drawlist()
+    sent = sent + sent38.drawlist()
+    sent = sent + sent39.drawlist()
+    sent = sent + sent40.drawlist()
+    sent = sent + sent41.drawlist()
+    sent = sent + sent42.drawlist()
+    sent = sent + sent43.drawlist()
+    sent = sent + sent44.drawlist()
+    sent = sent + sent45.drawlist()
+    sent = sent + sent46.drawlist()
+    return sent
