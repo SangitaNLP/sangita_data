1453f6d373ccc9cc920be2c895b456429b87c512
ighosh98 <indraneel.ghosh1998@gmail.com>
2018-06-28 01:31:15 +0530

    setup.py changed

--- setup.py
+++ setup.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-@author: djokester
-Samriddhi Sinha,
-IIT Kharagpur
-"""
 
 import sys
 from setuptools import setup
@@ -14,7 +9,7 @@ if sys.argv[-1] == 'setup.py':
     print()
 
 if sys.version_info[:2] < (3, 5):
-    print(('lingatagger requires Python version 3.5 or later (%d.%d detected).' %sys.version_info[:2]))
+    print(('sangita_data requires Python version 3.5 or later (%d.%d detected).' %sys.version_info[:2]))
     sys.exit(-1)
 
 sys.path.insert(0, 'sangita_data')
@@ -29,7 +24,7 @@ if __name__ == "__main__":
         description = "Data and Corpus pertaining to Sangita",
         url='https://github.com/djokester/sangita_data',
         keywords= ['nlp', 'hindi', 'linguistics'],
-        packages = ['sangita_data','sangita_data.hindi', 'sangita_data.hindi.sentences'],
+        packages = ['sangita_data','sangita_data.hindi', 'sangita_data.hindi.HindiDependencyTreeBank','sangita_data.hindi.Webscraping'],
         license = 'Apache License'
     )
 
